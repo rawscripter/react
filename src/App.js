160d5416e4a3f9d04b9025434fd60b392dee00a8
rawscripter <rawscripterx@gmail.com>
2019-01-01 02:54:34 +0600

    State added for Persons

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
+
+  state = {
+    persons : [
+      { name: 'shuvo', age : 13},
+      { name: 'Goswami', age : 24},
+      { name: 'Kumar', age : 33}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello World </h1>
-        <Person name="shuvo" age="18"> </Person>
-        <Person name="Goswami" age="20"> </Person>
+  
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> </Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I am Special</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> </Person>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const person = (props) => {
-    return <p> Hello I am {props.name} and i'm {props.age} years old </p>;
+    return (
+        <div>
+             <p> Hello I am {props.name} and i'm {props.age} years old </p>
+             <p>{props.children}</p>
+        </div>
+    );
 };
 
 
