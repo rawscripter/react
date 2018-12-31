c0b90d01f1384029d2d6c5ad79669fb121876636
rawscripter <rawscripterx@gmail.com>
2019-01-01 02:37:41 +0600

    Person Function imported in app.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello World </h1>
+        <Person />
       </div>
     );
   }
